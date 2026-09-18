eab7f1cffeab6eed8e1169d623c7bf012ae0f5d4
Donald Abishai <abishaifernando2907@gmail.com>
2026-09-19 00:57:05 +0530

    Remove old week2 assignment file

--- src/main/java/week2/WordReversalEncoder.java
+++
@@ -1,5 +0,0 @@
-import java.util.*;
-public class WordReversalEncoder {
- static String reverseEachWord(String s){String[] w=s.split(" ");StringBuilder r=new StringBuilder();for(String x:w)r.append(new StringBuilder(x).reverse()).append(" ");return r.toString().trim();}
- public static void main(String[] args){System.out.println(reverseEachWord(new Scanner(System.in).nextLine()));}
-}
