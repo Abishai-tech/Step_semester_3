edd9e1a828fe665647f7415ae9c10e672f1ca545
Donald Abishai <abishaifernando2907@gmail.com>
2026-09-19 11:22:31 +0530

    Add Week 5 access modifiers class problems

---
+++ src/main/java/access_Modifiers/class_problems/CineScreen.java
@@ -0,0 +1,27 @@
+package access_Modifiers.class_problems;
+
+public class CineScreen {
+    private final int seatsTotal;
+    private int seatsAvailable;
+
+    public CineScreen(int seatsTotal) {
+        if (seatsTotal <= 0)
+            throw new IllegalArgumentException("seatsTotal must be positive");
+        this.seatsTotal = seatsTotal;
+        this.seatsAvailable = seatsTotal;
+    }
+
+    public void bookSeat() {
+        if (seatsAvailable > 0)
+            seatsAvailable--;
+    }
+
+    public void cancelBooking() {
+        if (seatsAvailable < seatsTotal)
+            seatsAvailable++;
+    }
+
+    public int getSeatsAvailable() {
+        return seatsAvailable;
+    }
+}
