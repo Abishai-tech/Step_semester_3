3064c70f9e6cbb44374880b7888c825a2f55d5d3
Donald Abishai <abishaifernando2907@gmail.com>
2026-09-19 11:22:26 +0530

    Add Week 5 access modifiers class problems

---
+++ src/main/java/access_Modifiers/class_problems/MovieTicket.java
@@ -0,0 +1,15 @@
+package access_Modifiers.class_problems;
+
+public class MovieTicket {
+    private String seatNumber;
+    protected String screenId;
+    double ticketPrice;
+    public String movieTitle;
+
+    public MovieTicket(String seatNumber, String screenId, double ticketPrice, String movieTitle) {
+        this.seatNumber = seatNumber;
+        this.screenId = screenId;
+        this.ticketPrice = ticketPrice;
+        this.movieTitle = movieTitle;
+    }
+}
